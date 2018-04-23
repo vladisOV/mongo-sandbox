d68b31e3bf499a294fde9742eb9a6654c9f6f8af
Vladislav.Ovcharenko <Vladislav.Ovcharenko@x5.ru>
2018-04-23 17:37:40 +0300

    subdocuments posts

---
+++ users/test/subdocument_test.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Subdocuments", () => {
+  it("can create a subdocument", async () => {
+    const vasa = new User({
+      name: "Vasa",
+      posts: [{ title: "Post" }]
+    });
+    await vasa.save();
+    const user = await User.findOne({ name: "Vasa" });
+    assert(user.posts[0].title === "Post");
+  });
+  it("can add subdocument to an existing record", async () => {
+    var vasa = new User({
+      name: "Vasa",
+      posts: []
+    });
+    await vasa.save();
+    vasa = await User.findOne({ name: "Vasa" });
+    vasa.posts.push({ title: "PostTitle" });
+    await vasa.save();
+    vasa = await User.findOne({ name: "Vasa" });
+    assert(vasa.posts[0].title === "PostTitle");
+  });
+});
